b3be1fb49cd6250da78496106835ae8078315456
Gasthaus Alt Grieth <270933862+thedanyaproject-coder@users.noreply.github.com>
2026-03-26 13:35:12 +0100

    Create webhook.js

---
+++ api/webhook.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  return res.status(200).send("OK");
+}
